9f634bdf19421d732c174b6c809ebcd0588e7693
cml <chenmingliang@taptap.com>
2020-07-09 18:55:38 +0800

    Initial Commit

---
+++ WKDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WKDemo
+//
+//  Created by cml2 on 2020/7/9.
+//  Copyright © 2020 cml2. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
